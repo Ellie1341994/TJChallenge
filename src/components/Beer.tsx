c7f45238fd69b9625c827dc93a256e1a24fbe6c8
ellie <eli1341994@gmail.com>
2021-09-12 00:24:06 -0300

    Add Beer component layout
    Beer component presents a beer's relevant information

--- src/components/Beer.tsx
+++ src/components/Beer.tsx
@@ -1,15 +1,55 @@
 import * as React from "react";
-import { Beer as IBeer } from "../types/beer";
+import { Beer as IBeer, Ingredients as IIngredients } from "../types/beer";
 import { Image } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  Grid,
+  GridItem,
+  Text,
+  ListItem,
+  List,
+} from "@chakra-ui/react";
+const Ingredients = (ingredients: any): JSX.Element | null => {
+  return null;
+};
 export const Beer = ({
   name,
   description,
   tagline,
-  imageURL,
-  firstBrewed,
-  foodPairing,
-  brewersTips,
+  image_url,
+  first_brewed,
+  food_pairing,
+  brewers_tips,
   ingredients,
+  ...rest
 }: IBeer): JSX.Element => {
-  return <Image alt="A Beer" src={imageURL ?? undefined} />;
+  return (
+    <Flex direction="column" p="5%" height={{ base: "100vh" }} overflowY="auto">
+      <Heading textAlign="left">{name}</Heading>
+      <Text fontSize="sm">{tagline}</Text>
+      <Image
+        p="5%"
+        alt="A Beer"
+        boxSize="100vw"
+        objectFit="contain"
+        src={image_url ?? undefined}
+      />
+      <Text p="1%" fontSize="sm">
+        {description}
+      </Text>
+      <Heading fontSize="sm" pt="1%">
+        Food pairing
+      </Heading>
+      <List p="1%" fontSize="xs">
+        {food_pairing?.map((pair) => (
+          <ListItem>{pair}</ListItem>
+        ))}
+      </List>
+      <Heading fontSize="sm">Brewer tips</Heading>
+      <Text p="1%" fontSize="xs">
+        {brewers_tips}
+      </Text>
+    </Flex>
+  );
 };
