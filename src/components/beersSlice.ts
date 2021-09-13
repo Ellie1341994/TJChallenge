4457f5b954e072273dc67e5a0efe953a8608a835
ellie <eli1341994@gmail.com>
2021-09-13 16:49:58 -0300

    Add new async thunks for beer data fetching and their corresponding reducers
    Fix thunk typePrefix error for getBeer and getRandomBeer
    Removed unused imports

--- src/components/beersSlice.ts
+++ src/components/beersSlice.ts
@@ -1,10 +1,16 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { RootState, AppThunk } from "../app/store";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { beersService } from "../services/beers";
 import { Beer as IBeer } from "../types/beer";
 
-export const getRandomBeer = createAsyncThunk("beers/get", beersService.random);
+export const getRandomBeer = createAsyncThunk(
+  "beers/random",
+  beersService.random
+);
 export const getBeer = createAsyncThunk("beers/get", beersService.get);
+export const getBeersFilteredBy = createAsyncThunk(
+  "beers/filter",
+  beersService.filter
+);
 interface IBeersState {
   data: IBeer[];
   status: undefined | "loading" | "success" | "failed";
@@ -22,20 +28,22 @@ export const beersSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder
-      .addCase(getRandomBeer.pending, (state) => {
-        state.status = "loading";
-        state.message = "Loading data";
-      })
-      .addCase(getRandomBeer.fulfilled, (state, action) => {
-        state.status = "success";
-        state.data = action.payload;
-        state.message = "Data loaded";
-      })
-      .addCase(getRandomBeer.rejected, (state) => {
-        state.status = "failed";
-        state.message = "Loading data failed";
-      });
+    for (let beerRequest of [getBeer, getRandomBeer, getBeersFilteredBy]) {
+      builder
+        .addCase(beerRequest.pending, (state) => {
+          state.status = "loading";
+          state.message = "Loading data";
+        })
+        .addCase(beerRequest.fulfilled, (state, action) => {
+          state.status = "success";
+          state.data = action.payload;
+          state.message = "Data loaded";
+        })
+        .addCase(beerRequest.rejected, (state) => {
+          state.status = "failed";
+          state.message = "Loading data failed";
+        });
+    }
   },
 });
 
