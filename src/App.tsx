db6115a0419040e9f0cddae7b0f76ce5707b0853
ellie <eli1341994@gmail.com>
2021-09-13 16:17:32 -0300

    Change refactored "/" "/beers" routes into one and default route component.
    Component NotFoundView is now used instead of a mere string
    for default non-matching routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ import {
 import "./App.css";
 import { BeerView } from "./pages/BeerView";
 import { AuthView } from "./pages/AuthView";
-import { GroceryView } from "./pages/GroceryView";
+import { StoreView } from "./pages/StoreView";
+import { NotFoundView } from "./pages/NotFoundView";
 export const App = () => {
-  const isAuthenticated = localStorage.getItem("fakeToken");
   React.useEffect(() => {
     //return localStorage.removeItem("fakeToken");
   }, []);
@@ -19,19 +19,21 @@ export const App = () => {
     <Router>
       <Switch>
         <Route
-          path="/beers"
-          render={({ location }) => <GroceryView children={location.search} />}
-        />
-        <Route
           exact
-          path="/"
-          render={() => <Redirect to="beers?page=1&per_page=12" />}
+          path={["/", "/beers"]}
+          render={({ location }) =>
+            /beers$/.test(location.pathname) && location.search ? (
+              <StoreView children={location.search} />
+            ) : (
+              <Redirect to="beers?page=1&per_page=12" />
+            )
+          }
         />
         <Route
           exact
           path="/cart"
           render={({ location }) =>
-            isAuthenticated ? (
+            localStorage.getItem("fakeToken") ? (
               "Cart page"
             ) : (
               <Redirect to={`/login?from=${location.pathname}`} />
@@ -42,14 +44,12 @@ export const App = () => {
           exact
           path={["/login", "/register"]}
           render={({ location }) =>
-            isAuthenticated ? (
+            localStorage.getItem("fakeToken") ? (
               <Redirect to="/" />
             ) : (
               <AuthView
                 type={location.pathname}
-                to={
-                  new URLSearchParams(location.search).get("from") ?? "/store"
-                }
+                to={new URLSearchParams(location.search).get("from") ?? "/"}
               />
             )
           }
@@ -60,7 +60,14 @@ export const App = () => {
           path="/beer/:id"
           render={({ match }) => <BeerView beerId={match.params.id} />}
         />
-        <Route render={() => "Not found page"} />
+        <Route
+          render={({ location }) => (
+            <NotFoundView
+              title="Beer not found!"
+              message={`${location.pathname} was not found`}
+            />
+          )}
+        />
       </Switch>
     </Router>
   );
