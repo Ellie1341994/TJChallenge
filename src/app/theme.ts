98498413dab50a960def875a7b2ff702a591f6d9
ellie <eli1341994@gmail.com>
2021-09-13 12:24:35 -0300

    Change default app background to a light gray

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -3,4 +3,13 @@ export const theme = extendTheme({
   fonts: {
     heading: "Grenze Gotisch",
   },
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "gray.50",
+        color: "#333",
+      },
+    },
+  },
 });
