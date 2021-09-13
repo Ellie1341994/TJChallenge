6d69528fa63f56c6f39cff391874a95322277506
ellie <eli1341994@gmail.com>
2021-09-13 16:15:48 -0300

    Change ErrorBoard is further configurable with new props

--- src/components/ErrorBoard.tsx
+++ src/components/ErrorBoard.tsx
@@ -4,10 +4,25 @@ import {
   AlertTitle,
   AlertDescription,
 } from "@chakra-ui/react";
-export const ErrorBoard = ({ message }: { message: string }) => (
-  <Alert status="error" rounded="md" w={{ base: "90%", sm: "50%" }}>
+export const ErrorBoard = ({
+  asColumn = false,
+  title = "Error",
+  status = "error",
+  message,
+}: {
+  status?: "error" | "success" | "warning" | "info";
+  asColumn?: boolean;
+  title?: string;
+  message: string;
+}) => (
+  <Alert
+    status={status}
+    flexDirection={asColumn ? "column" : "row"}
+    rounded="md"
+    w={{ base: "90%", sm: "50%" }}
+  >
     <AlertIcon />
-    <AlertTitle mr={2}>Error </AlertTitle>
+    <AlertTitle mr={2}>{title} </AlertTitle>
     <AlertDescription>{message}</AlertDescription>
   </Alert>
 );
