9e482a2986f5defed13da39421d8fcb6d853f65d
ellie <eli1341994@gmail.com>
2021-09-14 16:09:15 -0300

    Add seenBeer and buyBeer reducers for history and buy features

--- src/components/beersSlice.ts
+++ src/components/beersSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { beersService } from "../services/beers";
 import { Beer as IBeer } from "../types/beer";
 import { PunkApiErrorResponse } from "../types/PunkAPI";
@@ -12,24 +12,34 @@ export const getBeersFilteredBy = createAsyncThunk(
   "beers/filter",
   beersService.filter
 );
+
 interface IBeersState {
   data: IBeer[];
   status: undefined | "loading" | "success" | "failed";
   history: string[];
-  bought: string[];
+  cart: string[];
   message: string;
 }
 const initialState: IBeersState = {
   data: [],
   history: [],
-  bought: [],
+  cart: [],
   status: undefined,
   message: "Preloaded data",
 };
 export const beersSlice = createSlice({
   name: "beersStorage",
   initialState,
-  reducers: {},
+  reducers: {
+    seeBeer: (state, action: PayloadAction<string>) => {
+      state.history = [...state.history, action.payload];
+    },
+    buyBeer: (state, action: PayloadAction<string>) => {
+      if (!state.cart.includes(action.payload)) {
+        state.cart = [...state.cart, action.payload];
+      }
+    },
+  },
   extraReducers: (builder) => {
     for (let beerRequest of [getBeer, getRandomBeer, getBeersFilteredBy]) {
       builder
@@ -51,5 +61,5 @@ export const beersSlice = createSlice({
     }
   },
 });
-
+export const { seeBeer, buyBeer } = beersSlice.actions;
 export const beersReducer = beersSlice.reducer;
